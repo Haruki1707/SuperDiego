9cfcf14c5c4991ef8d95aea31fa7759bcc74d96d
Diego Gómez <72423267+Haruki1707@users.noreply.github.com>
2024-02-26 10:47:39 -0600

    Create ProductStore + fix to model

--- src/models/Product.kt
+++ src/models/Product.kt
@@ -6,5 +6,5 @@ import helpers.Model
  * Models.Product class represents a product in the system.
  * It contains a name, quantity and price.
  */
-data class Products(val name: String, val quantity: Int, val price: Double): Model {
+data class Product(val name: String, val quantity: Int, val price: Double): Model {
 }
---
+++ src/stores/ProductStore.kt
@@ -0,0 +1,28 @@
+package stores
+
+import helpers.FileBase
+import helpers.Model
+import models.Product
+import java.io.File
+
+/**
+ * ProductsStore is an object that extends FileBase.
+ * It is responsible for handling operations related to Product data.
+ */
+object ProductStore: FileBase() {
+    override val file = File("C:\\GitHub\\SuperDiego\\resources\\products.txt")
+    val products = data as ArrayList<Product>
+
+    init {
+        baseInit()
+    }
+
+    override fun lineToModel(data: List<String>): Model {
+        return Product(data[0], data[1].toInt(), data[2].toDouble())
+    }
+
+    override fun modelToLine(model: Model): String {
+        val product = model as Product
+        return "${product.name},${product.quantity},${product.price}\n"
+    }
+}
