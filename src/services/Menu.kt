9a4cb6390d97d178bf06234941f07e2533f26004
Diego Gómez <72423267+Haruki1707@users.noreply.github.com>
2024-02-26 10:53:54 -0600

    Create Menu service

---
+++ src/services/Menu.kt
@@ -0,0 +1,57 @@
+package services
+
+import helpers.Role
+
+/**
+ * This object represents the Menu in the application.
+ * It contains a list of options, each with a corresponding action.
+ */
+object Menu {
+    private val options = ArrayList<Triple<String, () -> Unit, Int>>()
+
+    fun addOption(option: String, action: () -> Unit, role: Role) {
+        options.add(Triple(option, action, role.value))
+    }
+
+    fun show() {
+        println("Bienvenido ${Auth.user!!.username} | Rol: ${Auth.user!!.roleAsEnum().name}")
+        println("¿Qué desea hacer?")
+
+        var counter = 1
+        for (i in 0 until options.size) {
+            if (options[i].third <= Auth.user!!.roleAsEnum().value) {
+                println("${counter}. ${options[i].first}")
+                counter++
+            }
+        }
+        println("${counter}. Cerrar sesión")
+    }
+
+    private fun calculateOption(option: Int): Int {
+        var counter = 1
+        for (i in 0 until options.size) {
+            if (options[i].third <= Auth.user!!.roleAsEnum().value) {
+                if (counter == option) return i
+                counter++
+            }
+        }
+        if (option == counter) return -1
+        throw Exception("Invalid option.")
+    }
+
+    fun executeOption(option: String) {
+        try {
+            val index = calculateOption(option.toInt())
+            if (index == -1) {
+                Auth.logout()
+                println("Sesión cerrada. Presione enter para continuar.")
+                readln()
+            } else {
+                options[index].second()
+            }
+        } catch (e: Exception) {
+            println("Opción inválida. Presione enter para continuar.")
+            readln()
+        }
+    }
+}
