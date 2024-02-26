f0cb23bfcae10993f815d352c3acf8803067de4d
Diego Gómez <72423267+Haruki1707@users.noreply.github.com>
2024-02-26 10:56:24 -0600

    Modified main with docs and runApp method

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,9 @@
 fun main() {
-    println("Hello World!")
+    // Example of how to add options to the menu
+    // The first parameter is the name of the option
+    // The second parameter is the action to be executed when the option is selected
+    // The third parameter is the role required to execute the option
+    // Menu.addOption("Example", ExampleController::newExample, Role.ADMIN)
+
+    runApp()
 }
