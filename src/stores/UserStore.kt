17dc347059337113a40298f1a767d0c0b1a3a664
Diego Gómez <72423267+Haruki1707@users.noreply.github.com>
2024-02-26 10:47:15 -0600

    Create UserStore.kt

---
+++ src/stores/UserStore.kt
@@ -0,0 +1,28 @@
+package stores
+
+import helpers.FileBase
+import helpers.Model
+import models.User
+import java.io.File
+
+/**
+ * UserStore is an object that extends FileBase.
+ * It is responsible for handling operations related to User data.
+ */
+object UserStore: FileBase() {
+    override val file = File("C:\\GitHub\\SuperDiego\\resources\\users.txt")
+    val users = data as ArrayList<User>
+
+    init {
+        baseInit()
+    }
+
+    override fun lineToModel(data: List<String>): Model {
+        return User(data[0], data[1], data[2])
+    }
+
+    override fun modelToLine(model: Model): String {
+        val user = model as User
+        return "${user.username},${user.role},${user.password}\n"
+    }
+}
