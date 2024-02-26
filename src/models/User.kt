404c3ad635b36a12b0f965e49136e2cdb38d4005
Diego Gómez <72423267+Haruki1707@users.noreply.github.com>
2024-02-26 10:43:54 -0600

    Created User model with roleAsEnum

---
+++ src/models/User.kt
@@ -0,0 +1,20 @@
+package models
+
+import helpers.Model
+import helpers.Role
+import java.util.Locale
+
+/**
+ * Models.User class represents a user in the system.
+ * It contains a name, role and password.
+ */
+data class User(val username: String, val role: String, var password: String): Model {
+    fun roleAsEnum(): Role {
+        // Check if the role is valid, if not return INVITADO
+        return try {
+            Role.valueOf(role.uppercase(Locale.getDefault()))
+        } catch (e: IllegalArgumentException) {
+            Role.INVITADO
+        }
+    }
+}
