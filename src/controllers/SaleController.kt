1df25341f518b70714971894d1335353ba81ad2d
karivas-dev <karenrivasg17@gmail.com>
2024-03-08 23:29:31 -0600

    Update sale functionality

--- src/controllers/SaleController.kt
+++ src/controllers/SaleController.kt
@@ -1,6 +1,6 @@
 package controllers
 
-import models.Product
+import helpers.clearScreen
 import stores.ProductStore
 
 import java.io.File
@@ -8,13 +8,16 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 object SaleController {
-    private val items: ArrayList<String> = ArrayList()
+    private var items: ArrayList<String> = ArrayList()
 
     fun sale() {
         printInventory()
+        var footer = "¡Gracias por su compra!"
+        items = ArrayList()
+        var productId = -1
+        var total = 0.0
 
         val productRequest = readln()
-        var productId = -1
 
         productRequest.split(',').forEachIndexed { index, it ->
             var lineItem = ""
@@ -24,41 +27,74 @@ object SaleController {
                 ProductStore.products.find { p -> p.id == productId }?.let {product ->
                     if (product.quantity >= it.toInt()) {
                         product.quantity -= it.toInt()
-
-                        lineItem = "${product.id}\t${product.name}\t${product.quantity}\t${product.unit}\t${product.price}\t${ String.format("%.2f", product.price*product.quantity)}"
+                        total += String.format("%.2f", product.price*it.toInt()).toDouble()
+                        lineItem = "${product.id}\t${product.name}\t${it}\t${product.unit}\t${product.price}\t${total}"
                         items.add(lineItem)
+                    } else if (product.quantity > 0) {
+                        total += String.format("%.2f", product.price*product.quantity).toDouble()
+                        lineItem = "${product.id}\t${product.name}\t${product.quantity}\t${product.unit}\t${product.price}\t${total}"
+                        items.add(lineItem)
+                        product.quantity = 0
+                        footer = "\nAl parecer no tenemos las unidades solicitades en existencia.\nSe realizó la venta con las unidades disponibles\n¡Gracias por su compra!"
                     }
-                } ?: println("No existe un producto con el ID ${productId}")
-
+                    else println("No hay unidades en existencias de ${product.name}")
+                } ?: println("No existe un producto con el ID $productId")
             }
             else productId = it.toInt()
         };
 
-        ProductStore.saveFile()
-        generateTicket()
+        if (items.isNotEmpty()) {
+            ProductStore.saveFile()
+            generateTicket(total, footer)
+        }
+
         readln()
     }
 
     private fun printInventory() {
-        println("Nombre del producto \t Unidades disponibles \t Precio por unidad ")
+        clearScreen()
+        println("Nombre del producto \t Unidades disponibles \t Precio por unidad")
         ProductStore.products.forEach { p ->
-            println("${p.id}. ${p.name} \t\t ${p.quantity} ${p.unit} \t\t $${p.price}")
+            println("${p.id}. ${p.name} \t| ${p.quantity} ${p.unit} \t| $${p.price}")
         }
 
-        println("Ingrese los productos y cantidad que desea comprar, separados por comas")
+        println("Ingrese los productos y cantidad que desea comprar, separados por comas:")
     }
 
-    private fun printTicket() {
-
-    }
-
-    private fun generateTicket() {
+    private fun generateTicket(subtotal: Double, footer: String = "¡Gracias por su compra!") {
         val date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH.mm.ss"))
         val ticket = File("resources/tickets/Factura ${date}.txt")
 
-        ticket.appendText("FACTURA DIGITAL - SuperDiego\nFecha ${date} \n\n")
+        ticket.appendText("FACTURA DIGITAL - SuperDiego\nFecha $date \n\n")
         ticket.appendText("Cód\tProducto\t\tCantidad\t\tPrecio unitario\t\tTotal\n")
-        items.forEach { item -> ticket.appendText(item+"\n") }
+
+        println("\nFACTURA DIGITAL - SuperDiego")
+        println("Fecha $date \n")
+        println("Cód\tProducto\t\tCantidad\t\tPrecio unitario\t\tTotal")
+
+        items.forEach { item ->
+            ticket.appendText(item+"\n")
+            println(item)
+        }
+
+        if (subtotal > 20) {
+            val discount = subtotal * 0.03
+            val total = subtotal - discount
+            ticket.appendText("\nDescuento 3%: $${String.format("%.2f", discount)}\n")
+            ticket.appendText("Subtotal: $${String.format("%.2f", subtotal)}\n")
+            ticket.appendText("Total de factura: $${String.format("%.2f", total)}\n")
+
+            println("\nDescuento 3%: $${String.format("%.2f", discount)}")
+            println("Subtotal: $${String.format("%.2f", subtotal)}")
+            println("Total de factura: $${String.format("%.2f", total)}")
+        } else {
+            ticket.appendText("\nTotal de factura: $${String.format("%.2f", subtotal)}")
+            println("Total de factura: $${String.format("%.2f", subtotal)}")
+        }
+
         ticket.createNewFile()
+
+        println(footer)
+        println("\nVenta realizada con éxito. Se ha generado la factura ${ticket.name}")
     }
 }
